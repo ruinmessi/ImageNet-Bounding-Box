33e1b094f93674cd8a7965e8493b1c288900cfee
Sumukh Shivakumar <sumukhshiv@yahoo.com>
2016-07-30 16:16:13 -0400

    Ver 2. Contains all bounding box info

--- bounding_box.py
+++ bounding_box.py
@@ -16,6 +16,32 @@ for subdir, dirs, files in os.walk(rootdir):
                     root = ET.Element("annotation")
                     # doc = ET.SubElement(root, "doc")
                     x = os.path.splitext(x)[0]
-                    ET.SubElement(root, "folder").text = x
+                    ET.SubElement(root, "folder").text = d
+                    ET.SubElement(root, "filename").text = x
+                    source = ET.SubElement(root, "source")
+                    ET.SubElement(source, "database").text = "ImageNet database"
+                    size = ET.SubElement(root, "size")
+                    ET.SubElement(size, "width").text = str(width)
+                    ET.SubElement(size, "height").text = str(height)
+                    ET.SubElement(size, "depth").text = '3'
+                    ET.SubElement(root, "segmented").text = "0"
+                    object = ET.SubElement(root, "object")
+                    ET.SubElement(object, "name").text = d
+                    ET.SubElement(object, "pose").text = 'Unspecified'
+                    ET.SubElement(object, "truncated").text = '0'
+                    ET.SubElement(object, "difficult").text = '0'
+                    bndbox = ET.SubElement(object, "bndbox")
+                    ET.SubElement(bndbox, "xmin").text = '0'
+                    ET.SubElement(bndbox, "ymin").text = '0'
+                    ET.SubElement(bndbox, "xmax").text = str(width)
+                    ET.SubElement(bndbox, "ymax").text = str(height)
+
+
+
+
+
+
+
                     tree = ET.ElementTree(root)
+                    # tree.write(rootdir + '/'+ d+'/' + x +".xml", pretty_print=True)
                     tree.write(x +".xml", pretty_print=True)
